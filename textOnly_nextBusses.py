11534877fc75efac8e94ea4252a19090f225a422
shaham150 <efrat.shaham@protonmail.com>
2025-10-17 23:36:06 -0400

    Remove deprecated version of "nextBusses.py", create terminal-only version for testing

---
+++ textOnly_nextBusses.py
@@ -0,0 +1,8 @@
+# NextBusses - Text-only testing version
+
+import pandas as pd
+from google.transit import gtfs_realtime_pb2 as gtfs  # From the MobilityData > awesome-transit repo on GitHub
+
+
+
+
