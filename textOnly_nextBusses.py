b9deedd2438fb8d7838ec4167c0840265a4d3085
shaham150 <efrat.shaham@protonmail.com>
2025-10-24 23:06:43 -0400

    Finish functionality for finding the next listed arrival time at a selected stop (still working on discerning arrival time appropriate to trip direction)

--- textOnly_nextBusses.py
+++ textOnly_nextBusses.py
@@ -2,13 +2,17 @@
 
 import pandas as pd
 from google.transit import gtfs_realtime_pb2 as gtfs  # From the MobilityData > awesome-transit repo on GitHub
-
+from time import localtime
 
 # Import dataframe from file:
 try:
     df_route_info = pd.read_csv("staticRouteInfo.csv", low_memory=False)
 except:
     raise Exception("File 'staticRouteInfo.csv' not found! Try generating a new one with 'ParseStaticRouteInfo.py'?")
+
+# Convert time columns to datetime type:
+df_route_info["arrival_time"] = pd.to_datetime(df_route_info["arrival_time"], format="%H:%M:%S").dt.time  # Convert series to timestamps
+df_route_info["departure_time"] = pd.to_datetime(df_route_info["departure_time"], format="%H:%M:%S").dt.time  # Convert series to timestamps
 
 # Store non-numeric route names for later checks:
 non_numeric_route_names = [ name for name in df_route_info["route_short_name"].unique() if not name.isnumeric() ]
@@ -50,3 +54,9 @@ while not selected_stop.isnumeric() or int(selected_stop) < 1 or int(selected_st
 selected_stop = stops_list[int(selected_stop)-1]  # Store name of selected stop
 
 print(f"SELECTED: {selected_route}, {selected_stop}")
+
+
+curr_timestamp = pd.to_datetime(f"{localtime().tm_hour}:{localtime().tm_min}:{localtime().tm_sec}", format="%H:%M:%S").time()  # Create timestamp of current time
+next_time = df_route_info.loc[ (df_route_info["route_short_name"] == selected_route) & (df_route_info["stop_name"] == selected_stop) & (df_route_info["arrival_time"] >= curr_timestamp) ].sort_values(by="arrival_time")["arrival_time"].iloc[0]  # Find the next-closest arrival time (current-time inclusive)
+
+print(f"The next bus will arrive at: {next_time}")
